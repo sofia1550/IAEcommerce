760808887ba98db6035b650e319159bcd1f7a3c5
Tu Nombre <tuemail@example.com>
2024-05-27 03:07:40 -0300

    f gi

--- src/app/ecommerce/page.tsx
+++ src/app/ecommerce/page.tsx
@@ -12,7 +12,6 @@ const Ecommerce = () => {
   return (
     <>
       <Navbar />
-
       <Sidebar />
       <GlobalStyle />
       <Ecommercee />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 // Home.tsx
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import Footer from "./components/footer/footer";
 import { GlobalStyle } from "./globalStyle/globalStyle";
 import Header from "./components/header/header";
 import Navbar from "./components/navbar/navbar";
 import ProjectOverview from "./components/projectOverview/projectOverview";
-import ProductPreviewSection from "./components/products/productPreviewSection/productPreviewSection";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks"; // Asegúrate de ajustar la ruta de importación
+/* import ProductPreviewSection from "./components/products/productPreviewSection/productPreviewSection";
+ */ /* import { useAppDispatch, useAppSelector } from "@/redux/hooks"; // Asegúrate de ajustar la ruta de importación
 import {
   fetchProducts,
   selectVisibleProducts,
-} from "@/redux/features/product/productSlice"; // Asegúrate de que la ruta de importación sea correcta
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+} from "@/redux/features/product/productSlice"; // Asegúrate de que la ruta de importación sea correcta */
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 function Home() {
-  const dispatch = useAppDispatch();
-  const products = useAppSelector(selectVisibleProducts);
+  /*   const dispatch = useAppDispatch();
+  const products = useAppSelector(selectVisibleProducts); */
 
-  useEffect(() => {
+  /*   useEffect(() => {
     dispatch(fetchProducts());
-  }, [dispatch]);
+  }, [dispatch]); */
 
   return (
     <>
@@ -29,7 +29,8 @@ function Home() {
       <GlobalStyle />
       <Header />
       <ProjectOverview />
-      <ProductPreviewSection products={products} />
+      {/*       <ProductPreviewSection products={products} />
+       */}{" "}
       <Footer />
     </>
   );
